517786eef244bd0355e718dc870d247256f5776e
田浪 <1584731441@qq.com>
2021-07-28 20:28:43 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -12,86 +12,100 @@
       <Queue v-show="showQueue"></Queue>
     </transition>
     <Bottom></Bottom>
-    <div style="fontSize:12px; position: absolute; bottom: 10%;left:10px;zIndex: -1;">
-      备案号：<a href="https://beian.miit.gov.cn" target="_blank">浙ICP备19010344号-1</a>
-    </div>
   </div>
 </template>
 
 <script>
-import Top from './components/Top.vue'
-import Index from './components/Index.vue'
-import Bottom from './components/Bottom.vue'
-import SongDetail from './components/SongDetail.vue'
-import Queue from './components/Queue.vue'
+import Top from "./components/Top.vue";
+import Index from "./components/Index.vue";
+import Bottom from "./components/Bottom.vue";
+import SongDetail from "./components/SongDetail.vue";
+import Queue from "./components/Queue.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Top,
     Index,
     Bottom,
     SongDetail,
-    Queue
+    Queue,
   },
-  data(){
+  data() {
     return {
-      show:true,
-      showQueue:false
-    }
-  }
-}
+      show: true,
+      showQueue: false,
+    };
+  },
+};
 </script>
 
 <style>
-  *{
-    margin: 0;
-    padding: 0;
-  }
+* {
+  margin: 0;
+  padding: 0;
+}
 
-  #app {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-  }
+#app {
+  position: relative;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+#app::before {
+  position: absolute;
+  content: "";
+  display: block;
+  z-index: 0;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  background: url(./assets/imgs/bg.jpg);
+  background-size: cover;
+  filter: blur(10px);
+  transform: scale(1.1);
+  top: 0%;
+}
 
-  .header {
-    height: 60px;
-    position: relative;
-    top: 0;
-    left: 0;
-    right: 0;
-  }
+.header {
+  height: 60px;
+  position: relative;
+  top: 0;
+  left: 0;
+  right: 0;
+}
 
-  .bottom {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 60px;
-  }
+.bottom {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 60px;
+}
 
-  .index {
-    position: fixed;
-    top: 60px;
-    bottom: 60px;
-    left: 0;
-    right: 0;
-  }
+.index {
+  position: fixed;
+  top: 60px;
+  bottom: 60px;
+  left: 0;
+  right: 0;
+}
 
-  /* 去除scrollbar横向滚动条 */
-  .el-scrollbar__wrap {
-      overflow-x: hidden !important;
-  }
+/* 去除scrollbar横向滚动条 */
+.el-scrollbar__wrap {
+  overflow-x: hidden !important;
+}
 
-  .fade-enter-active, .fade-leave-active {
-      transition: transform .5s
-  }
-  .fade-enter, .fade-leave-active {
-    transform: translate3d(0,100%,0);
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 0.5s;
+}
+.fade-enter,
+.fade-leave-active {
+  transform: translate3d(0, 100%, 0);
+}
 
-  .el-loading-spinner .path {
-    stroke: rgb(236, 65, 65) !important;
-  }
+.el-loading-spinner .path {
+  stroke: rgb(236, 65, 65) !important;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,85 +1,76 @@
 import Vue from 'vue'
-import VueRouter from "vue-router"
-
-
-// import Discover from "@/views/Discover/Discover.vue"
-// import Recommend from '@/views/Recommend/Recommend.vue'
-// import NewSongs from '@/views/NewSongs/NewSongs.vue'
-// import MVs from '@/views/MVs/MVs.vue'
-// import Result from '@/views/Result/Result.vue'
-// import Playlist from '@/views/Playlist/Playlist.vue'
-// import MvDetail from '@/views/MvDetail/MvDetail.vue'
-// import Artist from '@/views/Artist/Artist.vue'
-// import Album from '@/views/Album/Album.vue'
+import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
 // 解决重复点击导航时，控制台出现报错
-const VueRouterPush = VueRouter.prototype.push;
+const VueRouterPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(to) {
-  return VueRouterPush.call(this, to).catch(err => err)
+  return VueRouterPush.call(this, to).catch((err) => err)
 }
 
 const routes = [
   {
-    path:"/",
-    redirect:'/discover'
+    path: '/',
+    redirect: '/discover',
   },
   {
-    path:"/discover",
+    path: '/discover',
     component: () => import('@/views/Discover/Discover.vue'),
   },
   {
-    path:"/recommend",
+    path: '/recommend',
     component: () => import('@/views/Recommend/Recommend.vue'),
     meta: {
-      keepAlive:true,
-    }
+      keepAlive: true,
+    },
   },
   {
-    path:"/newsongs",
+    path: '/newsongs',
     component: () => import('@/views/NewSongs/NewSongs.vue'),
   },
   {
-    path:"/mvs",
+    path: '/mvs',
     component: () => import('@/views/MVs/MVs.vue'),
     meta: {
-      keepAlive: true
-    }
+      keepAlive: true,
+    },
   },
   {
-    path:'/result',
-    component: () => import('@/views/Result/Result.vue')
+    path: '/result',
+    component: () => import('@/views/Result/Result.vue'),
   },
   {
-    path:'/playlist',
+    path: '/playlist',
     component: () => import('@/views/Playlist/Playlist.vue'),
   },
   {
-    path:'/mvdetail',
-    component: () => import('@/views/MvDetail/MvDetail.vue')
+    path: '/mvdetail',
+    component: () => import('@/views/MvDetail/MvDetail.vue'),
   },
   {
-    path:'/artist',
-    component: () => import('@/views/Artist/Artist.vue')
+    path: '/artist',
+    component: () => import('@/views/Artist/Artist.vue'),
   },
   {
-    path:'/album',
-    component: () => import('@/views/Album/Album.vue')
-  }
+    path: '/album',
+    component: () => import('@/views/Album/Album.vue'),
+  },
 ]
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
 })
 
-router.beforeEach((to,from,next)=>{
-  let needKeepAlive = ['/recommend','/mvs']
-  if(needKeepAlive.includes(to.path) && (from.path==="/mvdetail" || from.path === '/playlist'))
+router.beforeEach((to, from, next) => {
+  let needKeepAlive = ['/recommend', '/mvs']
+  if (
+    needKeepAlive.includes(to.path) &&
+    (from.path === '/mvdetail' || from.path === '/playlist')
+  )
     to.meta.keepAlive = true
-  else
-    to.meta.keepAlive = false
+  else to.meta.keepAlive = false
   next()
 })
 
